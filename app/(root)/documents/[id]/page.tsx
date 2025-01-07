bc0e1e8878d5303a2926fb94bf89d0565a27f359
SuhailaEltayeb <suhailaeltayeb@gmail.com>
2025-01-07 20:51:46 +0300

    Displaying header on top of the editor

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,11 +1,16 @@
 import { Editor } from '@/components/editor/Editor'
-import React from 'react'
+import Header from '@/components/Header'
 
 const Document = () => {
   return (
     <div>
+      <Header>
+        <div className="flex w-fit items-center justify-center gap-2">
+          <p className="document-title">This is a test</p>
+        </div>
+      </Header>
       <Editor />
-      </div>
+    </div>
   )
 }
 
