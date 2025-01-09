8ef4f752587b96468131fffe48ee0407462dea67
SuhailaEltayeb <suhailaeltayeb@gmail.com>
2025-01-10 02:07:34 +0300

    Setting up collaborative rooms and active collaborators

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,14 +1,31 @@
-import CollaborativeRoom from '@/components/CollaborativeRoom'
+import CollaborativeRoom from "@/components/CollaborativeRoom"
 import { Editor } from '@/components/editor/Editor'
 import Header from '@/components/Header'
+import { getDocument } from '@/lib/actions/room.actions'
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
+import { currentUser } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation'
 
-const Document = () => {
+const Document = async ({ params: { id } }: SearchParamProps) => {
+  const clerkUser = await currentUser();
+  if(!clerkUser) redirect('/sign-in');
+
+  const room = await getDocument({
+    roomId: id,
+    userId: clerkUser.emailAddresses[0].emailAddress,
+  }) 
+
+  if(!room) redirect('/');
+
+  //assess permission
+
   return (
-    <div>
-      <CollaborativeRoom
-      />
-    </div>
+    <main className="flex w-full flex-col items-center">
+      <CollaborativeRoom 
+        roomId={id}
+        roomMetadata={room.metadata}
+        />
+    </main>
   )
 }
 
--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -5,29 +5,45 @@ import { liveblocks } from '../liveblocks';
 import { revalidatePath } from 'next/cache';
 import { parseStringify } from '../utils';
 
-export const CreateDocument = async ({ userId, email }:
-CreateDocumentParams) => {
-    const roomId = nanoid();
+export const createDocument = async ({ userId, email }: CreateDocumentParams) => {
+  const roomId = nanoid();
 
-        try {
-            const metadata = {
-                creatorId: userId,
-                email,
-                title: 'Untitled'
-            }
+  try {
+    const metadata = {
+      creatorId: userId,
+      email,
+      title: 'Untitled'
+    }
 
-            const usersAccesses: RoomAccesses = {
-                [email] : ['room:write']
-            }
-            const room = await liveblocks.createRoom(roomId, {
-                metadata,
-                usersAccesses,
-                defaultAccesses: []
-              });
-            revalidatePath('/');
+    const usersAccesses: RoomAccesses = {
+      [email]: ['room:write']
+    }
 
-            return parseStringify(room);
-        } catch (error) {
-            console.log('Error happened while creating a room: ${error}');
-        }
-    } 
+    const room = await liveblocks.createRoom(roomId, {
+      metadata,
+      usersAccesses,
+      defaultAccesses: ['room:write']
+    });
+    
+    revalidatePath('/');
+
+    return parseStringify(room);
+  } catch (error) {
+    console.log(`Error happened while creating a room: ${error}`);
+  }
+}
+export const getDocument = async ({ roomId, userId }: { roomId: string; userId: string }) => {
+  try {
+      const room = await liveblocks.getRoom(roomId);
+    
+      const hasAccess = Object.keys(room.usersAccesses).includes(userId);
+    
+      if(!hasAccess) {
+        throw new Error('You do not have access to this document');
+      }
+    
+      return parseStringify(room);
+  } catch (error) {
+    console.log(`Error happened while getting a room: ${error}`);
+  }
+}
