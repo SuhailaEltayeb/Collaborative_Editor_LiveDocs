381648e40ce722c048c60b05ccad9cf7bff0832f
SuhailaEltayeb <suhailaeltayeb@gmail.com>
2025-01-23 01:37:49 +0300

    Implementing user editing/viewing permissions, plus liveblocks plugin to implement floating ans sticky comments on documents

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -2,6 +2,7 @@ import CollaborativeRoom from "@/components/CollaborativeRoom"
 import { Editor } from '@/components/editor/Editor'
 import Header from '@/components/Header'
 import { getDocument } from '@/lib/actions/room.actions'
+import { getClerkUsers } from "@/lib/actions/user.actions"
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
 import { currentUser } from '@clerk/nextjs/server'
 import { redirect } from 'next/navigation'
@@ -17,13 +18,26 @@ const Document = async ({ params: { id } }: SearchParamProps) => {
 
   if(!room) redirect('/');
 
-  //assess permission
+  const userIds = Object.keys(room.userAccesses);
+  const users = await getClerkUsers({ userIds });
+
+  const userData = users.map((user:User) => ({
+    ...user,
+    usertype: room.usersAccesses[user.email]?.includes('room:write')
+      ? 'editor'
+      : 'viewer'
+  }))
+
+  const currentUserType = room.usersAccesses[clerkUser.emailAddresses[0].emailAddress]?.includes
+  ('room:write') ? 'editor' : 'viewer';
 
   return (
     <main className="flex w-full flex-col items-center">
       <CollaborativeRoom 
         roomId={id}
         roomMetadata={room.metadata}
+        users={usersData}
+        CurrentUserType={currentUserType}
         />
     </main>
   )
