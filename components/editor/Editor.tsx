91de2e57c4c5f4fb5d15be49c868e1216b9971bd
SuhailaEltayeb <suhailaeltayeb@gmail.com>
2025-01-24 00:57:25 +0300

    Finalizing general document  comments on the right side of editor, with reply and mention options

--- components/editor/Editor.tsx
+++ components/editor/Editor.tsx
@@ -11,10 +11,11 @@ import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin';
 import { LexicalErrorBoundary } from '@lexical/react/LexicalErrorBoundary';
 import React from 'react';
 
-import { liveblocksConfig, LiveblocksPlugin, useEditorStatus } from '@liveblocks/react-lexical';
+import { FloatingComposer, FloatingThreads, liveblocksConfig, LiveblocksPlugin, useEditorStatus } from '@liveblocks/react-lexical';
 import Loader from '../Loader';
 import FloatingToolbarPlugin from './plugins/FloatingToolbarPlugin'
 import { useThreads } from '@liveblocks/react/suspense';
+import Comments from '../Comments';
 
 // Catch any errors that occur during Lexical updates and log them
 // or throw them as needed. If you don't throw them, Lexical will
@@ -66,6 +67,7 @@ export function Editor({ roomId, currentUserType }: {roomId: string, currentUser
           <LiveblocksPlugin>
             <FloatingComposer className="w-[350px]" />
             <FloatingThreads threads={threads} />
+            <Comments />
 
 
           </LiveblocksPlugin>
